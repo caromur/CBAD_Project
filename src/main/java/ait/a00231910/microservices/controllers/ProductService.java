5d2fcdd21e2c786a724a26313ab4414a1b34312c
caromur <adamcarolan96@gmail.com>
2022-04-19 23:46:21 +0100

    Testing Automatic Build and Maven Build2!

--- src/main/java/ait/a00231910/microservices/controllers/ProductService.java
+++ src/main/java/ait/a00231910/microservices/controllers/ProductService.java
@@ -38,7 +38,7 @@ public class ProductService {
 	@RequestMapping("/")
 	public String returnHello()
 	{
-		System.out.println("Testing Automatic Build and Maven Build!");
+		System.out.println("Testing Automatic Build and Maven Build2!");
 		return "Hello World";
 	}
 	
