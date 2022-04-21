0e34df62eb44411a494cc9a15fd4b83c1338aaf0
caromur <adamcarolan96@gmail.com>
2022-04-21 15:02:48 +0100

    Removed unnecessary imports from ProductDTO

--- src/main/java/ait/a00231910/microservices/dto/ProductDTO.java
+++ src/main/java/ait/a00231910/microservices/dto/ProductDTO.java
@@ -1,11 +1,5 @@
 package ait.a00231910.microservices.dto;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 public class ProductDTO {
 
 	private Long id;
