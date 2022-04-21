b5f0962c774304a834f65fbbc4279c77486dc03b
caromur <adamcarolan96@gmail.com>
2022-04-21 22:19:44 +0100

    Testing Copied Jobs

--- src/main/java/ait/a00231910/microservices/controllers/ProductController.java
+++ src/main/java/ait/a00231910/microservices/controllers/ProductController.java
@@ -41,7 +41,7 @@ public class ProductController {
 	@GetMapping("/")
 	public String returnHello()
 	{
-		System.out.println("Testing Jenkins SonarCloud again");
+		System.out.println("Testing Copied Jobs");
 		return productService.getHello();
 	}
 	
