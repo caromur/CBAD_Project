16eed3ee2f77608e1f980d5fdc88d0f84b935b8d
caromur <adamcarolan96@gmail.com>
2022-04-25 14:26:36 +0100

    Test Full Pipeline

--- src/main/java/ait/a00231910/microservices/controllers/ProductController.java
+++ src/main/java/ait/a00231910/microservices/controllers/ProductController.java
@@ -41,7 +41,7 @@ public class ProductController {
 	@GetMapping("/")
 	public String returnHello()
 	{
-		System.out.println("Testing Full Pipeline1");
+		System.out.println("Testing Full Pipeline2");
 		return productService.getHello();
 	}
 	
